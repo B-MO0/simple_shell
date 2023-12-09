511eef1bea3386e6444594e423155ecc37162363
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2023-12-09 06:26:43 -0800

    shell

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+/* allowed functions header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <dirent.h>
