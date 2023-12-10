48210c611b104c6af18447856014cd17c304dd7a
B-MO0 <144631945+B-MO0@users.noreply.github.com>
2023-12-10 22:31:37 +0100

    shell

---
+++ find_command.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+* find_command_in_path - Searches for the command in the directories.
+* @cmd: command.
+*
+* Return: Path to the command if found, if not NULL.
+*/
+char *find_command_in_path(char *cmd)
+{
+char *path = getenv("PATH");
+char *path_copy = strdup(path); // Duplicate PATH to avoid strtok modifying it
+if (!path_copy)
+{
+perror("strdup");
+return NULL;
+}
+
+char *full_path = malloc(MAX_LINE);
+if (!full_path)
+{
+perror("malloc");
+free(path_copy);
+return NULL;
+}
+
+char *dir = strtok(path_copy, ":");
+while (dir != NULL)
+{
+snprintf(full_path, MAX_LINE, "%s/%s", dir, cmd);
+if (access(full_path, X_OK) == 0)
+{
+free(path_copy);
+return full_path; // Found the command
+}
+dir = strtok(NULL, ":");
+}
+
+free(path_copy);
+free(full_path);
+return NULL; // Command not found
+}
