ba197964d2e5aacfa2cf071ac265f200bff72a85
B-MO0 <144631945+B-MO0@users.noreply.github.com>
2023-12-16 07:32:03 +0100

    wired

---
+++ prompt.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * prompt - mode check
+ * @fd: file stream
+ * @buf: buffer
+ **/
+void prompt(int fd, struct stat buf)
+{
+	fstat(fd, &buf);
+
+	if (S_ISCHR(buf.st_mode))
+		_puts(PROMPT);
+}
+
+/**
+ * _puts - print without a \n
+ * @str: string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+	unsigned int length;
+
+	length = _strlen(str);
+
+	write(STDOUT_FILENO, str, length);
+}
